880e5d9c0f181a20466d2dc311a84cf0ecf29076
Alexander Vukovic <alex.vukovic@portalinsturments.com>
2016-12-26 08:22:36 -0500

    Added import statements for test

--- utility.py
+++ utility.py
@@ -3,4 +3,5 @@
 Utility file for ETN!. utility.py consists of helper functions pertaining to
 string manipulation, as well as file generation, reading, and writing.
 """
-
+import markovify 
+import tweepy 
