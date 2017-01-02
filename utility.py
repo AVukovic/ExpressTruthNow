b51aaed9f5300edea907639c8a90dbe8dd8c569b
Alexander Vukovic <alex.vukovic@portalinsturments.com>
2017-01-02 14:46:07 -0500

    Added information fetching function to utility.py

--- utility.py
+++ utility.py
@@ -1,9 +1,72 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 26 08:37:42 2016
 
-@author: alexv
+"""
+utility.py is a collection of methods that assist in reading, writing, and 
+formatting text strings, such as tweets.
+
+
 """
 
-import markovify
-import tweepy
+def returninfo(address, info="ALL"):
+    """ 
+    Reads the info file provided, and returns relevant information based on 
+    user request.
+    
+    Args:
+        path: file destination (str)
+        
+        info: specific piece of information to return. Potential information to
+        return includes:
+            "USER": returns username
+            "PASS": returns password
+            "API": returns API key    
+            "ALL": returns username, password, and API
+    """
+    infofile = open(address, 'r').read() #opens file, returns as string
+    
+    if (info == "USER"): #elseif chain based on input
+        try:
+            start = infofile.index("(user)") + len("(user)")
+            end = infofile.index("(userend)")
+            return infofile[start:end]
+        except ValueError:
+            print "Error: Invalid File Format"
+    
+    elif (info == "PASS"):
+        try:
+            start = infofile.index("(pass)") + len("(pass)")
+            end = infofile.index("(passend)")
+            return infofile[start:end]
+        except ValueError:
+            print "Error: Invalid File Format"
+            
+    elif (info == "API"):
+        try:
+            start = infofile.index("(api)") + len("(api)")
+            end = infofile.index("(apiend)")
+            return infofile[start:end]
+        except ValueError:
+            print "Error: Invalid File Format"
+            
+    elif (info == "ALL"):
+        try:
+            userstart = infofile.index("(user)") + len("(user)")
+            userend = infofile.index("(userend)")
+            print ("USERNAME: " + infofile[userstart:userend])
+            
+            passstart = infofile.index("(pass)") + len("(pass)")
+            passend = infofile.index("(passend)")
+            print ("PASSWORD: " + infofile[passstart:passend])
+            
+            apistart = infofile.index("(api)") + len("(api)")
+            apiend = infofile.index("(apiend)")
+            print ("API: " + infofile[apistart:apiend])
+        except ValueError:
+            print "Error: Invalid File Format"
+            
+    else: print ("Error: Invalid File Format")
+
+            
+        
+        
+    
+            
